818b42150f61622e666d4970e38f889ec0a6fbea
CigaretteLizard <113203837+CigaretteLizard@users.noreply.github.com>
2023-10-13 19:03:45 -0400

    Update mw.ps1
    updated 10/13/23, ah ah ah

--- mw.ps1
+++ mw.ps1
@@ -1,5 +1,5 @@
 #CIGARETTE LIZARD
 
-invoke-WebRequest -Uri 'https://github.com/CigaretteLizard/MagicWord/raw/main/mw.mp4' -OutFile 'mw.mp4'
+invoke-WebRequest -Uri 'https://github.com/CigaretteLizard/MagicWord/raw/main/mw.mp4' -OutFile .\'mw.mp4'
 
 .\mw.mp4
