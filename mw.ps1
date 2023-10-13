571aaff98192e32124a71ae6c4d5e171fe8269e4
CigaretteLizard <113203837+CigaretteLizard@users.noreply.github.com>
2023-10-13 19:12:21 -0400

    Update mw.ps1
    ah ah ah, you didn't say the magic word

--- mw.ps1
+++ mw.ps1
@@ -1,5 +1,5 @@
 #CIGARETTE LIZARD
 
-invoke-WebRequest -Uri 'https://github.com/CigaretteLizard/MagicWord/raw/main/mw.mp4' -OutFile .\'mw.mp4'
+invoke-WebRequest -Uri 'https://github.com/CigaretteLizard/MagicWord/raw/main/mw.mp4' -OutFile c:\mw\'mw.mp4'
 
-.\mw.mp4
+C:\mw\mw.mp4
